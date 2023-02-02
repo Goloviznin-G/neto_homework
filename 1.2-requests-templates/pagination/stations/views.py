4dc088898fed40d7dd2bcd950ba36330341d2d6c
Goloviznin-G <goloviznin.opk@gmail.com>
2023-02-02 17:04:42 +0100

    video2

---
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -0,0 +1,22 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import csv
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+content = []
+with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        content.append(row)
+def bus_stations(request):
+    page_number = int(request.GET.get('page',1))
+    paginator = Paginator(content, 10)
+    page = paginator.get_page(page_number)
+    context = {
+        'bus_stations': page
+    }
+    return render(request, 'stations/index.html', context)
